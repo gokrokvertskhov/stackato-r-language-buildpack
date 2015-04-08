1f4397fb16767272b9fc7607aada6eb8a58f31f9
gokrokvertskhov <gokrokvertskhov@mirantis.com>
2015-04-07 20:04:15 -0700

    Fix several issues with bash Add run_R.sh wrapper

--- bin/run_R.sh
+++ bin/run_R.sh
@@ -2,6 +2,7 @@ export APP_ROOT=$HOME
 export LD_LIBRARY_PATH=$APP_ROOT/root/usr/lib:$APP_ROOT/root/usr/lib/R/lib:$LD_LIBRARY_PATH
 export R_HOME=$APP_ROOT/root/usr/lib/R
 export PATH=$APP_ROOT/root/usr/lib/R/bin:$PATH
+export APP_ROOT_DIR=$APP_ROOT
 
 $APP_ROOT/root/usr/lib/R/bin/R $APP_ROOT/src/main.r
 
